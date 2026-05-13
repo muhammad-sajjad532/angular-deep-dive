a26257d37c256689ef96ac2f5fb7be535c8f8979
Muhammad Sajjad <muhammadsajjad.0532@gmail.com>
2026-05-13 14:58:38 +0500

    feat: RxJS basics - Observable, Subscribe, Memory Leak

---
+++ rxjs-demo/src/app/app.ts
@@ -0,0 +1,42 @@
+import { Component, signal, OnDestroy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable, Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App implements OnDestroy {
+  protected readonly title = signal('rxjs-demo');
+
+  currentValue = 0;
+  private subscription: Subscription | null = null;
+
+  startTimer() {
+    // interval() har second ek value emit karta hai
+    const timer$ = interval(1000);
+
+    this.subscription = timer$.subscribe({
+      next: (val) => {
+        this.currentValue = val;
+        console.log('Value:', val);
+      },
+      error: (err) => console.log('Error:', err),
+      complete: () => console.log('Done!')
+    });
+  }
+
+  stopTimer() {
+    // Unsubscribe — stream band karo (memory leak rokne ke liye)
+    this.subscription?.unsubscribe();
+    console.log('Timer stopped!');
+  }
+
+  ngOnDestroy() {
+    // Component destroy ho to bhi unsubscribe karo
+    this.subscription?.unsubscribe();
+  }
+
+}
